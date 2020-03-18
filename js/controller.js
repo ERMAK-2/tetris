b0d4b9ce81ed7aad2633d90594f95be19089d54b
ERMAK-2 <57044652+ERMAK-2@users.noreply.github.com>
2020-03-18 20:34:08 +0300

    ЛЯ-ля-ЛЯ

---
+++ js/controller.js
@@ -0,0 +1,22 @@
+﻿document.body.onkeydown = function(e) {
+  var keys = { //Клавиши
+    37: 'left', // Стрелка влево
+    65: 'left', // 'A'
+
+    39: 'right', // Стрелка вправо
+    68: 'right', // 'D'
+
+    40: 'down', // Стрелка вниз
+    32: 'down', // Вниз - пробелом
+    83: 'down', // 'S'
+
+    38: 'rotate', // Вращение- стрелкой вверх
+    87: 'rotate', // 'W'
+
+    27: 'escape' // Пауза по клавише Esc
+  };
+  if (typeof(keys[e.keyCode]) != 'undefined') { //Если код клавиши допустимый,
+    keyPress(keys[e.keyCode]); //Передать его обработчику
+    render(); //и перерисовать стакан
+  }
+};
---
+++ js/theme.js
@@ -0,0 +1,17 @@
+﻿
+let page = document.querySelector('.page');
+let canva = document.querySelector('.canvas');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+
+  canva.classList.toggle('light');
+  canva.classList.toggle('dark');
+};
+
+function showhide(obj){
+    if(obj == 'none') return 'inline';
+    else return 'none';
+}
